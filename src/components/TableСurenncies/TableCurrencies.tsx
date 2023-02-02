968eab7af1bd1bbbbce36bfac7677cc200beba5f
Myou5uf <myxamed12l@gmail.com>
2023-02-02 20:34:38 +0700

    Fix the currency search

--- src/components/TableСurenncies/TableCurrencies.tsx
+++ src/components/TableСurenncies/TableCurrencies.tsx
@@ -18,24 +18,31 @@ function TableCurrencies(props: IProps) {
 
     const [searchValue, setSearchValue] = useState('');
     const [visibleAllCurrencies, setVisibleAllCurrencies] = useState(false);
-
+    const [visibleButtonLoadMore, setVisibleButtonLoadMore] = useState(true);
     const handleClickButtonLoadMore = useCallback(() => setVisibleAllCurrencies((prevState) => !prevState), []);
+    const handleChangeInput = useCallback((value: string) => {
+        setSearchValue(value);
+        // если запрос есть
+        if (value !== '') {
+            setVisibleAllCurrencies(true); // показать все возможные валюты
+            setVisibleButtonLoadMore(false); // скрыть кнопку - 'показать ещё'
+        } else {
+            setVisibleAllCurrencies(false); // запретить показ всех возможных валют
+            setVisibleButtonLoadMore(true); // показать кнопку - 'показать ещё'
+        }
+    }, []);
 
-    // возвращает отфильтрованные валюты (их ключи), если они есть, в виде массива, пример: ['AUD', 'AZN'] иначе возвращает переданные данные data
     const filteredCurrencies = useFilteredCurrencies(data, searchValue.trim());
 
     const getVisibleCurrencies = useCallback(() => {
-        // если есть отфильтрованные валюты, то возвращаем их
-        if (Array.isArray(filteredCurrencies) && filteredCurrencies.length > 0) return filteredCurrencies;
-        // иначе если НЕ нужно показывать все валюты, возвращаем первые NUMBER_OF_CURRENCIES_PER_PAGE валют
+        // если не нужно показывает все валюты
         if (!visibleAllCurrencies) {
-            return Object.keys(filteredCurrencies)
-                .slice(0, NUMBER_OF_CURRENCIES_PER_PAGE);
+            // вернуть первые NUMBER_OF_CURRENCIES_PER_PAGE валют
+            return filteredCurrencies.slice(0, NUMBER_OF_CURRENCIES_PER_PAGE);
         }
-
-        // иначе возвращаем ключи всех валют
-        return Object.keys(data);
-    }, [data, filteredCurrencies, visibleAllCurrencies]);
+        // иначе вернуть ключи всех валют
+        return filteredCurrencies;
+    }, [filteredCurrencies, visibleAllCurrencies]);
 
     return (
         <div className='tableCurrencies'>
@@ -44,43 +51,49 @@ function TableCurrencies(props: IProps) {
                 placeholder='Поиск валюты по коду (ISO 4217) и названию'
                 className='tableCurrencies__input'
                 value={searchValue}
-                onChange={(event) => setSearchValue(event.target.value)}
+                onChange={(event) => handleChangeInput(event.target.value)}
             />
             <div className='tableCurrencies__tableWrapper'>
-                <table className='table w-100'>
-                    <thead className='table__head'>
-                        <tr className='table__headRow'>
-                            {headerNames.map((name) => <th key={name} className='table__headItem'>{name}</th>)}
-                        </tr>
-                    </thead>
-                    <tbody className='table__body'>
-                        {getVisibleCurrencies()
-                            .map((currency, i) => (
-                                <TableCurrenciesRow
-                                    key={currency}
-                                    index={i + 1}
-                                    isoCode={data[currency].NumCode}
-                                    nominal={data[currency].Nominal}
-                                    nameCurrency={data[currency].Name}
-                                    currencyRUB={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.RUB].Value)
-                                        .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
-                                    currencyUSD={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.USD].Value)
-                                        .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
-                                    currencyEUR={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.EUR].Value)
-                                        .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
-                                    currencyCNY={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.CNY].Value)
-                                        .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
-                                />
-                            ))}
-                    </tbody>
-                </table>
+                {filteredCurrencies?.length > 0
+                    ? (
+                        <table className='table w-100'>
+                            <thead className='table__head'>
+                                <tr className='table__headRow'>
+                                    {headerNames.map((name) => <th key={name} className='table__headItem'>{name}</th>)}
+                                </tr>
+                            </thead>
+                            <tbody className='table__body'>
+                                {getVisibleCurrencies()
+                                    .map((currency, i) => (
+                                        <TableCurrenciesRow
+                                            key={currency}
+                                            index={i + 1}
+                                            isoCode={data[currency].NumCode}
+                                            nominal={data[currency].Nominal}
+                                            nameCurrency={data[currency].Name}
+                                            currencyRUB={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.RUB].Value)
+                                                .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
+                                            currencyUSD={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.USD].Value)
+                                                .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
+                                            currencyEUR={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.EUR].Value)
+                                                .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
+                                            currencyCNY={convertCurrency(data[currency].Value, data[BaseCurrenciesTypes.CNY].Value)
+                                                .toFixed(NUMBER_OF_DIGITS_AFTER_POINT)}
+                                        />
+                                    ))}
+                            </tbody>
+                        </table>
+                    )
+                    : <p className='tableCurrencies__notFound'>Такой валюты в нашей базе нет...</p>}
             </div>
-            <Button
-                className='tableCurrencies__btn'
-                onClick={handleClickButtonLoadMore}
-            >
-                {visibleAllCurrencies ? 'Скрыть' : 'Показать все'}
-            </Button>
+            {visibleButtonLoadMore && (
+                <Button
+                    className='tableCurrencies__btn'
+                    onClick={handleClickButtonLoadMore}
+                >
+                    {visibleAllCurrencies ? 'Скрыть' : 'Показать все'}
+                </Button>
+            )}
         </div>
     );
 }
--- src/hooks/useFilteredCurrencies.ts
+++ src/hooks/useFilteredCurrencies.ts
@@ -2,23 +2,24 @@ import {useMemo} from 'react';
 import {ICurrencies} from '../models/Currency';
 
 /**
- * Отфильтровать полученные валюты currencies по введеному запросу query.
- * Если запроса нет вернуть currencies.
+ * Отфильтровать полученные валюты currencies по введеному запросу.
+ * Вернуть ключи currencies, которые удовлетворяют запроку.
+ * Если запроса нет вернуть все ключи currencies.
  * Поиск ведется по коду и названию валюты.
  * @param currencies объект с валютами
  * @param query значение запроса из input
  */
-export const useFilteredCurrencies = (currencies: ICurrencies, query: string): ICurrencies | string[] => useMemo(() => {
+export const useFilteredCurrencies = (currencies: ICurrencies, query: string): string[] => useMemo(() => {
+    const currenciesKeys = Object.keys(currencies); // ключи всех полученных валют
     // если запрос есть
     if (query !== '') {
-        const currenciesKeys = Object.keys(currencies); // ключи валют
-        const lowerCaseQuery = query.toLowerCase(); // перенос запроса в нижний регистр
-        // фильтрация ключей по названию и коду и возврат отфильтрованных ключей по запросу
+        const lowerCaseQuery = query.toLowerCase(); // перенести запрос в нижний регистр
+        // вернуть отфильтрованные ключи по названию и коду
         return currenciesKeys.filter((key) => currencies[key].Name.toLowerCase()
             .includes(lowerCaseQuery)
             || currencies[key].NumCode.toLowerCase()
                 .includes(lowerCaseQuery));
     }
-    // иначе вернуть полученные данные - currencies
-    return currencies;
+    // иначе вернуть ключи всех полученных валют
+    return currenciesKeys;
 }, [currencies, query]);
